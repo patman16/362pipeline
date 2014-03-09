a71a3a2dd0f6f78793fea4522a00662644e9f429
Rachel Beth Stonehirsch <rbs335@bane.ece.northwestern.edu>
2014-03-09 14:50:28 -0500

    mem-unit

--- mem_unit.v
+++ mem_unit.v
@@ -1,48 +1,72 @@
-module MEMRegister(clk, dExtOp, dALUSrc, dRegDst, dMemWr, dBranch, dMemtoReg, dRegWr, dDsize, dZero, dALUout, qExtOp, qALUSrc, qRegDst, qMemWr, qBranch, qMemtoReg, qRegWr, qDsize, qZero, qALUout);
-	input clk, dExtOp, dALUSrc, dRegDst, dMemWr, dBranch, dMemtoReg, dRegWr, dZero;
-	input [1:0] dDsize;
-	input [31:0] dALUout;
-	output qExtOp, qALUSrc, qRegDst, qMemWr, qBranch, qMemtoReg, qRegWr, qZero;
-	output [1:0] qDsize;
-	output [31:0] qALUout;
-	reg [1:0] qDsize;
-	reg [31:0] qALUout;
-	reg qExtOp,qALUSrc,qRegDst, qMemWr, qBranch, qMemtoReg, qRegWr, qZero;
+module MEMRegister(clk, dMemWr, dBranch, dMemtoReg, dRegWr, dDsize, dZero, dExecResult, dBusB, dloadext, dJump, dJal, dJar,dFPoint, dRw, dBranchTarget, qMemWr, qBranch, qMemtoReg, qRegWr, qDsize, qZero, qExecResult, qBusB, qloadext, qJump, qJal, qJar, qFPoint, qRw, qBranchTarget);
+	input clk, dMemWr, dBranch, dMemtoReg, dRegWr, dZero, dloadext, dJump, dJal, dJar;
+	input [1:0] dDsize, dFPoint;
+	input [4:0] dRw;
+	input [31:0] dExecResult, dBusB, dBranchTarget;
+	output qMemWr, qBranch, qMemtoReg, qRegWr, qZero, qloadext, qJump, qJal, qJar;
+	output [1:0] qDsize, qFPoint;
+	output [4:0] qRw;
+	output [31:0] qExecResult, qBusB, qBranchTarget;
+	reg [1:0] qDsize, qFPoint;
+	reg [4:0] qRw;
+	reg [31:0] qExecResult;
+	reg qMemWr, qBranch, qMemtoReg, qRegWr, qZero, qJump, qJal, qJar;
 
 	initial begin
-		qExtOp <= 0;
-		qALUSrc <= 0;
-		qRegDst <= 0;
 		qMemWr <=0;
 		qBranch <= 0;
 		qMemtoReg <= 0;
 		qRegWr <= 0;
 		qDsize <= 0;
 		qZero <= 0;
-		qALUout <= 0;
-		
+		qExecResult <= 0;
+		qBusB <= 0;
+		qloadext <= 0;
+		qJump <=0;
+		qJal <= 0;
+		qJar <= 0;
+		qFPoint <= 0;
+		qRw <= 0;	
 	end
 
 	always @ (posedge clk)
 	begin
-		qExtOp <= dExtOp;
-		qALUSrc <= dALUSrc;
-		qRegDst <= dRegDst;
 		qMemWr <=dMemWr;
 		qBranch <= dBranch;
 		qMemtoReg <= dMemtoReg;
 		qRegWr <= dRegWr;
 		qDsize <= dDsize;
 		qZero <= dZero;
-		qALUout <= dALUout;
-
+		qExecResult <= dExecResult;
+		qBusB <= dBusB;
+		qloadext <= dloadext;
+		qJump <= dJump;
+		qJal <= dJal;
+		qJar <= dJar;
+		qFPoint <= dFPoint;
+		qRw <= dRw;
 	end
 endmodule
 
-module mem_unit();
+module mem_unit(clk, busB, loadext, memWr, dSize, execResult, branch, zero, branchTarget, memtoreg, regWr, jump, fPoint, jal, jar, rw, toIfetch, memtoreg_out, regWr_out, dmem_out, execResult_out, jump_out, fPoint_out, jal_out, jar_out, rw_out, PCSRC);
+
+	input clk, loadext, memWr,branch, zero, regWr, jump, jal, jar;
+	input [31:0] execResult, branchTarget, busB;
+	input [4:0] rw;
+	input [1:0] fPoint, dSize;
+	output memtoreg_out, regWr_out, jump_out,jal_out, jar_out;
+	output [31:0] dmem_out, execResult_out, PCSRC;
+	output [1:0] fPoint_out;
+	output [4:0] rw_out;
+	
+	wire [31:0] execResult_inside;
+	
+	
+	MEMRegister mRegister(clck, memWr, branch, memtoreg, regwr, zero, execResult, busB, loadext, jump, jal, jar, fPoint, rw, branchTarget, 
+	
 
 endmodule
-
+/*module MEMRegister(clk, dMemWr, dBranch, dMemtoReg, dRegWr, dDsize, dZero, dExecResult, dBusB, dloadext, dJump, dJal, dJar,dFPoint, dRw, dBranchTarget, qMemWr, qBranch, qMemtoReg, qRegWr, qDsize, qZero, qExecResult, qBusB, qloadext, qJump, qJal, qJar, qFPoint, qRw, qBranchTarget);*/
 
 /*  Need control signals for dmem
     dmem #(.SIZE(16384)) DMEM(alu_out, rData, datamem_muxin, memwrite, dsize, clock);
